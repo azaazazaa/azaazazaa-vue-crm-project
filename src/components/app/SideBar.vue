ffd92f98eec807cf99d825ef42f43df2451e0a8e
azaazazaa <56756165+azaazazaa@users.noreply.github.com>
2021-07-01 13:53:21 +0500

    Add files via upload

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: isOpen}">
+      <router-link
+        v-for="link in links"
+        :key="link.url"
+        tag="li"
+        active-class="active"
+        :to="link.url"
+        exact
+      >
+        <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+      </router-link>
+    </ul>
+</template>
+
+<script>
+export default {
+  props: ['isOpen'],
+  data: () => ({
+    links: [
+      { title: 'Счет', url: '/' },
+      { title: 'История', url: '/history' },
+      { title: 'Планирование', url: '/planning' },
+      { title: 'Новая запись', url: '/record' },
+      { title: 'Категория', url: '/categories' }
+    ]
+  })
+}
+</script>
